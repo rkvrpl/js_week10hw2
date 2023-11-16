ff2597abed127dc6e82f4feae7c555a710b28261
rkvrpl <rkvrpl@icloud.com>
2023-11-16 21:55:12 +0300

    done homework

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+const imgOne = document.querySelector(".image")
+const next = () =>{
+imgOne.src = "assets/img/mem2.jpg"
+};
+
+const imgTwo = document.querySelector(".image")
+const back = () =>{
+imgTwo.src = "assets/img/mem1.jpg"
+};
